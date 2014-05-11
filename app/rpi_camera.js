1957129134bfa388dfb69fd356015305ce472010
Shaba Abhiram <shabarivas@gmail.com>
2014-05-11 13:44:19 -0700

    updated image cmd line

--- app/rpi_camera.js
+++ app/rpi_camera.js
@@ -24,12 +24,12 @@ module.exports = function(log, storage_dir) {
         },
 
         _take_picture = function(image_path, options, callback) {
-            var cmd = "raspistill -t 0 -n -o \"" + image_path + "\"";
+            var cmd = "raspistill -t 1 -n -rot 180 -o \"" + image_path + "\"";
             // var cmd = "curl http://upload.wikimedia.org/wikipedia/commons/0/09/Polar_Bear_-_Alaska.jpg -o \"" + image_path + "\"";
             log.info("About to take pic:");
             log.info(cmd);
             exec(cmd, function(error, stderr, stdout) {
-                callback(error);
+                callback();
             });
         },
 
