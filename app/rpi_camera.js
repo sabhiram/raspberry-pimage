6204a4af66d956f9e8256167cd47d9f0b8d73bcc
Shaba Abhiram <shabarivas@gmail.com>
2014-05-11 13:38:12 -0700

    adding raspistill link to take_pic

--- app/rpi_camera.js
+++ app/rpi_camera.js
@@ -24,7 +24,8 @@ module.exports = function(log, storage_dir) {
         },
 
         _take_picture = function(image_path, options, callback) {
-            var cmd = "curl http://upload.wikimedia.org/wikipedia/commons/0/09/Polar_Bear_-_Alaska.jpg -o \"" + image_path + "\"";
+            var cmd = "raspistill -t 0 -n -o \"" + image_path + "\"";
+            // var cmd = "curl http://upload.wikimedia.org/wikipedia/commons/0/09/Polar_Bear_-_Alaska.jpg -o \"" + image_path + "\"";
             log.info("About to take pic:");
             log.info(cmd);
             exec(cmd, function(error, stderr, stdout) {
