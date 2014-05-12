71abc34367716db2b2e2e4231ca4259bed684872
Shaba Abhiram <shabarivas@gmail.com>
2014-05-11 20:37:04 -0700

    simple style for image stuff

--- app/public/js/app.controller.js
+++ app/public/js/app.controller.js
@@ -50,6 +50,66 @@ function AppController(AlbumManager, ImageManager, $scope) {
     });
 }
 
+app.directive("pimImagePreview", function() {
+    return {
+        restrict: "E",
+
+        scope: {
+            albumName: "@",     // Setup 2-way binding between the "value"
+                                // attribute and the data elm associated w/ it
+
+            imageName: "@",     // Setup text binding for the class we wish to 
+                                // assign this div
+        },
+
+        replace: true,          // Use provided template (as opposed to static
+                                // content that the modal scope might define in the
+                                // DOM)
+
+        transclude: false,      // Custom content provided in the template is
+                                // available in the directive.
+
+        template: [
+            "<div class='pim-image-container' ng-mouseenter='hovered = true' ng-mouseleave='hovered = false'>",
+            "    <img ng-src='/gallery/{{albumName}}/{{imageName}}' class='pim-image-img pim-center-image'></img>",
+            "    <div class='pim-image-toolbox' ng-class='{\"hide_toolbox\": !hovered}' align='right'>",
+            "        <i class='fa fa-search'></i>",
+            "        <i class='fa fa-edit'></i>",
+            "        <i class='fa fa-trash-o'></i>",
+            "    </div>",
+            "</div>"
+        ].join("\n"),
+
+        // Link function to bind modal to the app
+        link: function(scope, element, attributes) {
+            scope.toolbox_visible = false;
+
+            console.log("pimImagePreview link called");
+        },
+    };
+});
+
+/*
+<pim-image-preview album-name="selected_album.name" image-name="image.name" width="100%" height="100%"></pim-image-preview>
+
+*/
+
+app.directive('pimHover', function () {
+    return {
+        restrict: 'A',
+        scope: {
+            pimHover: '@'
+        },
+        link: function(scope, element) {
+            element.on('mouseenter', function() {
+                element.addClass(scope.pimHover);
+            }).on('mouseleave', function() {
+                element.removeClass(scope.pimHover);
+            });
+        }
+    };
+});
+
 app.service("AlbumManager", function($http) {
     return {
         list_albums: function() {
