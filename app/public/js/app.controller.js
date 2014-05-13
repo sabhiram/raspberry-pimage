9fd5c58c736c8e09ec30d4697dd971169ecc50bc
Shaba Abhiram <shabarivas@gmail.com>
2014-05-13 00:34:30 -0700

    adding routes for album management

--- app/public/js/app.controller.js
+++ app/public/js/app.controller.js
@@ -6,7 +6,52 @@
 ***/
 var app = angular.module("PIMageApp", [
     "ngAnimate",
+    "ngRoute"
 ]);
+
+app.config(["$routeProvider", function($routeProvider) {
+    $routeProvider.
+        when("/", {
+            templateUrl:    "templates/preview.html",
+            controller:     "PreviewController"
+        }).
+        when("/album/:album_name", {
+            templateUrl:    "templates/album.html",
+            controller:     "AlbumController"
+        }).
+        otherwise({
+            redirectTo:     "/"
+        });
+}]);
+
+/******************************************************************************\
+Service:
+    AlbumManager
+
+Dependencies:
+    $http
+
+Description:
+    This is a simple service which abstracts the HTTP ReST API which the
+    server exposes. This is useful since the service returns a bunch of
+    promise objects which can be chained easily with .then().success() etc
+\******************************************************************************/
+app.service("AlbumManager", function($http) {
+    return {
+        list_albums: function() {
+            return $http.get("/api/list_albums");
+        },
+        take_picture: function(album_name) {
+            return $http.get("/api/take_picture/album/" + album_name);
+        },
+        delete_image_from_album: function(album_name, image_name) {
+            return $http.delete("/api/album/" + album_name + "/image/" + image_name);
+        },
+        list_images_in_album: function(album_name) {
+            return $http.get("/api/list_images/album/" + album_name);
+        }
+    };
+});
 
 /******************************************************************************\
 Function:
@@ -18,20 +63,61 @@ Dependencies:
 Description:
     Main application controller
 \******************************************************************************/
-function AppController(AlbumManager, $scope) {
-
-    $scope.selected_album = null;
+function AppController(AlbumManager, $scope, $location) {
+    console.log("Main contoller loaded");
+    $scope.selected_album_name = null;
     $scope.albums = [];
-    $scope.images = [];
-    
+
     // Deferred load the list of albums and set the scope
     // appropriately.
     AlbumManager.list_albums().then(function(response) {
+        console.log("Main contoller loaded album info");
         $scope.albums = response.data;
     });
 
+    // // Watch when the album changes, and load the images from it...
+    // $scope.$watch("selected_album", function(new_value, old_value) {
+    //     if(new_value === old_value) {
+    //         return;
+    //     }
+
+    //     if(new_value) {
+    //         AlbumManager.list_images_in_album(new_value.name).then(function(response) {
+    //             $scope.images = response.data;
+    //         });
+    //     } else {
+    //         // Null new_value - clear the images??
+    //         $scope.images = [];
+    //     }
+    // });
+}
+
+function AlbumController($scope, $routeParams, AlbumManager) {
+    
+    var
+        album_name = $routeParams.album_name;
+
+    console.log("AlbumController loaded with name: " + album_name);
+
+    $scope.images = [];
+
+    AlbumManager.list_images_in_album(album_name).then(function(response) {
+        $scope.images = response.data;
+    });
+    
+    // Delete Image API
+    $scope.delete_image = function(index, image_name) {
+        AlbumManager.delete_image_from_album(album_name, image_name).then(function(response) {
+            if(response.data.status == "SUCCESS") {
+                $scope.images.splice(index, 1);
+            } else {
+                console.log("Error - unable to delete image");
+            }
+        });
+    }
+
     $scope.take_picture = function() {
-        AlbumManager.take_picture($scope.selected_album.name).then(function(response) {
+        AlbumManager.take_picture(album_name).then(function(response) {
             // The response is expected to send back a list of images in the album
             // so we can reload this page. Perhaps this needs to be de-coupled.
             // TODO: This should return just the image dict we want to append to {images}
@@ -45,40 +131,16 @@ function AppController(AlbumManager, $scope) {
             }
         });
     }
-
-    // Watch when the album changes, and load the images from it...
-    $scope.$watch("selected_album", function(new_value, old_value) {
-        if(new_value === old_value) {
-            return;
-        }
-
-        if(new_value) {
-            AlbumManager.list_images_in_album(new_value.name).then(function(response) {
-                $scope.images = response.data;
-            });
-        } else {
-            // Null new_value - clear the images??
-            $scope.images = [];
-        }
-    });
-
-    // Delete Image API
-    $scope.delete_image = function(index, album_name, image_name) {
-        console.log("app controller delete_image called" + album_name + " " + image_name);
-        AlbumManager.delete_image_from_album(album_name, image_name).then(function(response) {
-            if(response.data.status == "SUCCESS") {
-                $scope.images.splice(index, 1);
-            } else {
-                console.log("Error - unable to delete image");
-            }
-        });
-    }
-
-    
 }
 
+
+function PreviewController($scope) {
+    console.log("PreviewController loaded");
+}
+
+
 /******************************************************************************\
-Director:
+Directive:
     pimImageThumb <pim-image-thumb>
 
 Dependencies:
@@ -115,10 +177,9 @@ app.directive("pimImageThumb", function() {
                                 // available in the directive.
 
         template: [
-
             "<div class='pim-image-thumb' ng-mouseenter='hovered = true' ng-mouseleave='hovered = false'>",
             "    <div class='pim-image-header' ng-class='{\"selected\": hovered}'>{{imageName}}</div>",
-            "    <img ng-src='/gallery/{{albumName}}/{{imageName}}' class='pim-image-img pim-center-image'></img>",
+            "    <img ng-src='gallery/{{albumName}}/{{imageName}}' class='pim-image-img pim-center-image'></img>",
             "    <div class='pim-image-toolbox' ng-class='{\"hide_toolbox\": !hovered}' align='right'>",
             "        <i class='fa fa-search'></i>",
             "        <i class='fa fa-edit'></i>",
@@ -135,31 +196,34 @@ app.directive("pimImageThumb", function() {
     };
 });
 
-/******************************************************************************\
-Service:
-    AlbumManager
-
-Dependencies:
-    $http
-
-Description:
-    This is a simple service which abstracts the HTTP ReST API which the
-    server exposes. This is useful since the service returns a bunch of
-    promise objects which can be chained easily with .then().success() etc
-\******************************************************************************/
-app.service("AlbumManager", function($http) {
+app.directive("pimDropdown", function($location) {
     return {
-        list_albums: function() {
-            return $http.get("/api/list_albums");
+        restrict: "E",
+
+        scope: {
+            selection:   "=", // 2-way binding to the selected item
+            albums:      "=", // 2-way
+            prompt:      "@", // text binding
         },
-        take_picture: function(album_name) {
-            return $http.get("/api/take_picture/album/" + album_name);
+
+        replace: true,
+
+        template: [
+            "<div class='pim-dropdown-container' ng-class='{\"pim-expanded\": expanded}'>",
+            "    <div class='pim-dropdown-header' ng-click='expanded = !expanded'>{{prompt}}&nbsp;<i class='fa fa-chevron-down'></i></div>",
+            "    <div class='pim-dropdown-item' ng-repeat='album in albums' ng-click='change_album_to(album.name)'>",
+            "        {{ album.name }}",
+            "    </div>",
+            "</div>",
+        ].join("\n"),
+
+        link: function(scope, element, attributes) {
+            scope.expanded = false;
+            scope.change_album_to = function(album_name) {
+                scope.expanded = false;
+                scope.selection = album_name;
+                $location.url("/album/" + album_name);
+            }
         },
-        delete_image_from_album: function(album_name, image_name) {
-            return $http.delete("/api/album/" + album_name + "/image/" + image_name);
-        },
-        list_images_in_album: function(album_name) {
-            return $http.get("/api/list_images/album/" + album_name);
-        }
     };
 });
