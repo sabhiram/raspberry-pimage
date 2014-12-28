a1ea1644745eb9d7a66d46c8be633915143926cc
Shaba Abhiram <shabarivas@gmail.com>
2014-12-27 21:25:31 -0800

    moat tests and more coverage

--- test/rpi_camera.test.js
+++ test/rpi_camera.test.js
@@ -62,5 +62,31 @@ describe("RPI Camera Tests", function() {
             next_test();
         });
 
+        it("Build simple options - negative boolean", function(next_test) {
+            var result = rpi_camera.get_cmd_from_options({"a": false, "b": 100, "c": "hello"});
+            result.should.not.containEql("--a");
+            result.should.containEql("--b 100");
+            result.should.containEql("--c hello");
+            next_test();
+        });
+
+        it("Validate settings", function(next_test) {
+            var settings = rpi_camera.get_settings_sync();
+            var default_settings = rpi_camera.get_default_settings_sync();
+            settings.should.eql(default_settings);
+            next_test();
+        });
+
+        it("Save settings", function(next_test) {
+            var settings = rpi_camera.get_settings_sync();
+            settings["foobar"] = "1234";
+            rpi_camera.save_settings(settings, function(error) {
+                var new_settings = rpi_camera.get_settings_sync(),
+                    cmd_line     = rpi_camera.get_cmd_from_options(new_settings);
+                cmd_line.should.containEql("--foobar 1234");
+                next_test(error);
+            });
+        });
+
     });
 });
