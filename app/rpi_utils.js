edc4b8ba0771999e84c80cd09e1da933718f9e6c
Shaba Abhiram <shabarivas@gmail.com>
2014-12-28 19:05:48 -0800

    add empty impl for rpi_utils

---
+++ app/rpi_utils.js
@@ -0,0 +1,13 @@
+var
+    _     = require("underscore")._,
+    async = require("async"),
+    util  = require("util"),
+    path  = require("path");
+
+module.exports = function(log) {
+
+    return {
+
+    };
+
+}
---
+++ test/rpi_utils.test.js
@@ -0,0 +1,8 @@
+/*****************************************************************************\
+Validate RPI Utility Module, see file: ../app/rpi_utils.js
+\*****************************************************************************/
+describe("RPI Utils Tests", function() {
+
+    var rpi_utils = require("../app/rpi_utils")();
+
+});
